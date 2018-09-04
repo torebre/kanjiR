d4337214a9afa954ce18b34456d139b8c501a887
Tore Brede <tore.brede@gmail.com>
2018-09-04 22:23:40 +0200

    Backing up work

---
+++ cluster_experiments.R
@@ -0,0 +1,91 @@
+library(scatterplot3d)
+source('CreateLine.R')
+source('DrawLines.R')
+source('DrawLinePair.R')
+source('GenerateSecondLine.R')
+source('CreateLineMatrix.R')
+
+# unicode, relative_length, angle_diff, start_pair_distance, relative_distance
+#file.name <- '/home/student/workspace/testEncodings/position_data_2.csv'
+
+# unicode, relative_length, angle_diff, start_pair_distance, relative_distance, start_pair_angle_diff
+file.name <- '/home/student/workspace/testEncodings/position_data_4.csv'
+
+number.of.fields =max(count.fields(file.name, sep = ','))
+
+dat <- read.table(file.name, header = TRUE, 
+                  sep = ",",
+                  colClasses=rep("numeric", number.of.fields),
+                  fill = TRUE)
+
+lines <- list()
+for(i in 1:dim(position.data)[1]) {
+  lines[[i]] <- GenerateSecondLine(position.data[i, 2], position.data[i, 3], position.data[i, 4], position.data[i, 6])
+}
+
+
+number.of.points <- sum(sapply(lines, function(x) {
+  length(x)
+}))
+
+line.points <- matrix(nrow = number.of.points, ncol = 2)
+counter <- 1
+for(i in 1:length(lines)) {
+  line.points[counter, 1] <- lines[[i]][1]
+  line.points[counter, 2] <- lines[[i]][2]
+  counter <- counter + 1
+}
+
+
+line.matrix.pca <- prcomp(line.points[1:10000, ], center = F, scale. = F)
+biplot(line.matrix.pca)
+line.matrix.pca$rotation
+
+
+line.endpoints <- matrix(nrow = 2 * length(lines), ncol = 2)
+counter <- 1
+for(i in 1:(2 * length(lines))) {
+  line.endpoints[counter, 1] <- lines[[i]][1]
+  line.endpoints[counter, 2] <- lines[[i]][2]
+  counter <- counter + 1
+  
+  line.endpoints[counter, 1] <- lines[[i]][1]
+  line.endpoints[counter, 2] <- lines[[i]][2]
+  counter <- counter + 1
+}
+
+
+line.matrix.pca <- prcomp(line.points[1:10000, ], center = F, scale. = F)
+biplot(line.matrix.pca)
+line.matrix.pca$rotation
+
+
+DrawLines(lines)
+GenerateLineHeatMap(lines)
+
+
+line.matrix <- CreateLineMatrix(lines)
+
+min(line.matrix)
+max(line.matrix)
+
+
+filtered.line.vector <- as.vector(line.matrix)
+
+filtered.line.vector[which(filtered.line.vector < top.percentile)] <- 0
+
+filtered.line.vector2 <- as.vector(line.matrix)
+filtered.line.vector2 <- filtered.line.vector2[which(filtered.line.vector < top.percentile)]
+
+
+top.percentile.matrix.indices <- which(line.matrix > top.percentile, arr.ind = T)
+top.percentile.pca <- prcomp(top.percentile.matrix.indices, center = F, scale. = F)
+
+
+dims <- dim(line.matrix)
+filtered.line.matrix <- matrix(filtered.line.vector, dims[1], dims[2])
+
+
+filtered.line.pca <- prcomp(filtered.line.matrix, center = F, scale. = F)
+
+
