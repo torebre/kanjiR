93892a2e96a3b1859424d840ebd21fec41a2a4fd
Tore Brede <tore.brede@gmail.com>
2018-10-07 20:39:19 +0200

    Updating function for heat map

--- line_heatmap.R
+++ line_heatmap.R
@@ -1,59 +1,35 @@
-all.lines.in.kanji <-kanji.line.data[which(kanji.line.data[, 1] == 26397), ]
+source('CreateLine.R')
+source('DrawLineRelativeToInputLine.R')
+source('GenerateSecondLine.R')
+source('GenerateLineHeatMap.R')
+source('CreateLineMatrix.R')
 
-dim(lines.in.kanji)
+file.name <- '/home/student/workspace/testEncodings/kanji_data_full.csv'
+kanji.line.data <- read.table(file.name, header = T, sep = ",")
 
-par(mfrow=c(2, 3))
+kanji.unicodes <- unique(kanji.line.data[ , 1])
 
 lines <- list()
 counter <- 1
-for(i in 1:dim(all.lines.in.kanji)[1]) {
-  line1.original <- all.lines.in.kanji[i, ]
-  
-  if(line1.original$length < 10) {
-    next
-  }
-  
-  lines.draw <- DrawLineRelativeToInputLine(line1.original, all.lines.in.kanji, c(i))
+for(i in 1:length(kanji.unicodes)) {
+  all.lines.in.kanji <- kanji.line.data[which(kanji.line.data[, 1] == kanji.unicodes[i]), ]
 
-  for(j in 1:length(lines.draw)) {
-    lines[[counter]] <- lines.draw[[j]]
-    counter <- counter + 1
+  for(j in 1:dim(all.lines.in.kanji)[1]) {
+    line1.original <- all.lines.in.kanji[j, ]
+    
+    if(line1.original$length < 10) {
+      next
+    }
+    
+    lines.draw <- DrawLineRelativeToInputLine(line1.original, all.lines.in.kanji, skip = c(j))
+    for(j in 1:length(lines.draw)) {
+      lines[[counter]] <- lines.draw[[j]]
+      counter <- counter + 1
+    }
   }
-  
-  # print(paste('Highlighted line: ',i))
-  # print(lines.draw[[i]])
-  # DrawLines(lines.draw, highlighted.lines =  c(i), transpose = T, x.offset = 70, y.offset = 50, min.x = 0, min.y = 0, max.x = 100, max.y = 100)
 }
-
-
 
 GenerateLineHeatMap(lines)
-
 line.matrix <- CreateLineMatrix(lines)
-
-
-# for(i in 1:10) {
-#   line.matrix[1, i] <- 0
-# }
-
+truehist(line.matrix, prob = F)
 image(line.matrix)
-
-
-
-for(i in 1:6) { #length(lines)) {
-  DrawLines(lines[[i]])
-}
-
-
-par(mfrow=c(1, 1))
-DrawLineKanji(26397, kanji.line.data)
-
-DrawLinesInKanji(all.lines.in.kanji[1:8, ])
-
-test.line <- matrix(nrow = 1, ncol = 6)
-test.line[1, ] <- c(1, 0, 2, 10, 0, 0)
-test.line.data <- as.data.frame(test.line)
-colnames(test.line.data) <- c('unicode', 'line_number', 'angle', 'length', 'start_x', 'start_y')
-
-lines.draw <- DrawLineRelativeToInputLine(test.line.data, all.lines.in.kanji)
-DrawLines(lines.draw, transpose = T)
